58dc40cff832baf819cba9602a570fc347f74f90
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-18 23:38:14 +0200

    refactor(c): use shift operators for pangram kata

--- c/pangram/pangram.c
+++ c/pangram/pangram.c
@@ -1,26 +1,21 @@
 #include <stddef.h>
 #include <ctype.h>
+#include <stdint.h>
 #include "pangram.h"
 
 bool is_pangram(const char* sentence) {
     if (!sentence) return false;
-    bool alphabet[26] = {false};
 
+    uint32_t result = 0;
     while (*sentence) {
         if (isalpha(*sentence)) {
             const size_t i = islower(*sentence)
-                             ? *sentence - 'a'
-                             : *sentence - 'A';
-            alphabet[i] = true;
+                             ? 1 << (*sentence - 'a')
+                             : 1 << (*sentence - 'A');
+            result |= i;
         }
         sentence++;
     }
 
-    bool result = true;
-    for (size_t i = 0; i < 26; ++i) {
-        if (!alphabet[i])
-            result = false;
-    }
-
-    return result;
+    return result == 67108863;
 }

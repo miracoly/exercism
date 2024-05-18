1b763f269e7b745f98558d628caa580e5262dc21
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-18 17:02:32 +0200

    feat(c): add add raindrops kata

---
+++ c/raindrops/raindrops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "raindrops.h"
+
+void convert(char result[], int drops) {
+    if (!result) return;
+    if (drops % 3 == 0)
+        strcat(result, "Pling");
+    if (drops % 5 == 0)
+        strcat(result, "Plang");
+    if (drops % 7 == 0)
+        strcat(result, "Plong");
+    if (!*result)
+        sprintf(result, "%d", drops);
+}

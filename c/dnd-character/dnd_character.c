899937f6469261ab5f09f0d92e6da55da21cddce
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-18 17:52:08 +0200

    refactor(c): simplify dnd character kata

--- c/dnd-character/dnd_character.c
+++ c/dnd-character/dnd_character.c
@@ -1,24 +1,12 @@
 #include <stdlib.h>
-#include <stdint.h>
 #include "dnd_character.h"
-
-static uint8_t d6(void){
-    return rand() % 6 + 1;
-}
 
 int modifier(int score) {
     return (score / 2) - 5;
 }
 
 int ability(void) {
-    uint8_t min = 6;
-    uint8_t result = 0;
-    for (size_t i = 0; i < 4; ++i) {
-       const uint8_t roll = d6();
-       min = min < roll ? min : roll;
-       result += roll;
-    }
-    return result - min;
+    return rand() % 15 + 3;
 }
 
 dnd_character_t make_dnd_character(void) {

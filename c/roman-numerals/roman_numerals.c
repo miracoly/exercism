31c6d473e56ed4b041b20e3ff2a4963b302ab5c6
miracoly <68049792+miracoly@users.noreply.github.com>
2025-08-24 21:02:18 +0200

    refactor(c): improve roman numerals

--- c/roman-numerals/roman_numerals.c
+++ c/roman-numerals/roman_numerals.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LEN 15
+
 static const int base[] = {1,  4,   5,   9,   10,  40,  50,
                            90, 100, 400, 500, 900, 1000};
 static const char* symbol[] = {"I",  "IV", "V",  "IX", "X",  "XL", "L",
                                "XC", "C",  "CD", "D",  "CM", "M"};
+static const int symbolLen[] = {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1};
 
 char* to_roman_numeral(unsigned int number) {
-  char* out = calloc(1, 100);
+  if (number >= 4000) return NULL;
+
+  char* out = malloc(MAX_LEN + 1);
+  if (!out) return NULL;
 
   int i = sizeof(base) / sizeof(base[0]) - 1;
+  char* cur = out;
   while (number > 0) {
     int div = number / base[i];
     while (div) {
-      strcat(out, symbol[i]);
+      memcpy(cur, symbol[i], symbolLen[i]);
       div--;
+      cur += symbolLen[i];
     }
     number = number % base[i];
     i--;
   }
-
-  return realloc(out, strlen(out));
+  *cur = '\0';
+  char* tmp = realloc(out, cur - out + 1);
+  return tmp ? tmp : out;
 }

8004c97e33bd0e72d8095bfd6a156a479c88ad20
miracoly <68049792+miracoly@users.noreply.github.com>
2026-06-30 21:48:15 +0200

    feat: add reactor kata

--- c/react/react.c
+++ c/react/react.c
@@ -1,5 +1,7 @@
 #include "react.h"
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define REACT_INITIAL_CAPACITY 10
@@ -14,6 +16,20 @@ struct reactor {
   struct cell** cells;
 };
 
+static bool add_cell(struct reactor* r, struct cell* c) {
+  if (r->count == r->capacity) {
+    const size_t new_cap = r->capacity * 2;
+    struct cell** new_cs = realloc(r->cells, new_cap * sizeof *r->cells);
+    if (!new_cs) return false;
+
+    r->capacity = new_cap;
+    r->cells = new_cs;
+  }
+
+  r->cells[r->count++] = c;
+  return true;
+}
+
 struct reactor* create_reactor(void) {
   struct reactor* r = malloc(sizeof *r);
   if (!r) return NULL;
@@ -27,4 +43,18 @@ struct reactor* create_reactor(void) {
   r->count = 0;
   r->capacity = REACT_INITIAL_CAPACITY;
   return r;
+}
+
+struct cell* create_input_cell(struct reactor* r, int initial_value) {
+  if (!r) return NULL;
+
+  struct cell* c = malloc(sizeof *c);
+  if (!c) return NULL;
+
+  c->value = initial_value;
+  if (!add_cell(r, c)) {
+    free(c);
+    return NULL;
+  }
+  return c;
 }

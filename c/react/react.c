5c8c689086a5d4cfd3eaafd690c9f12cf42cbd90
miracoly <68049792+miracoly@users.noreply.github.com>
2026-07-01 11:55:41 +0200

    feat: add reactor kata

--- c/react/react.c
+++ c/react/react.c
@@ -1,4 +1,5 @@
 #include "react.h"
+#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -16,19 +17,7 @@ struct reactor {
   struct cell** cells;
 };
 
-static bool add_cell(struct reactor* r, struct cell* c) {
-  if (r->count == r->capacity) {
-    const size_t new_cap = r->capacity * 2;
-    struct cell** new_cs = realloc(r->cells, new_cap * sizeof *r->cells);
-    if (!new_cs) return false;
-
-    r->capacity = new_cap;
-    r->cells = new_cs;
-  }
-
-  r->cells[r->count++] = c;
-  return true;
-}
+static bool add_cell(struct reactor* r, struct cell* c);
 
 struct reactor* create_reactor(void) {
   struct reactor* r = malloc(sizeof *r);
@@ -45,6 +34,16 @@ struct reactor* create_reactor(void) {
   return r;
 }
 
+void destroy_reactor(struct reactor* r) {
+  if (!r) return;
+
+  for (size_t i = 0; i < r->count; ++i) {
+    free(r->cells[i]);
+  }
+  free(r->cells);
+  free(r);
+}
+
 struct cell* create_input_cell(struct reactor* r, int initial_value) {
   if (!r) return NULL;
 
@@ -57,4 +56,28 @@ struct cell* create_input_cell(struct reactor* r, int initial_value) {
     return NULL;
   }
   return c;
+}
+
+int get_cell_value(struct cell* c) {
+  assert(c != NULL);
+  return c->value;
+}
+
+void set_cell_value(struct cell* c, int new_value) {
+  assert(c != NULL);
+  c->value = new_value;
+}
+
+static bool add_cell(struct reactor* r, struct cell* c) {
+  if (r->count == r->capacity) {
+    const size_t new_cap = r->capacity * 2;
+    struct cell** new_cs = realloc(r->cells, new_cap * sizeof *r->cells);
+    if (!new_cs) return false;
+
+    r->capacity = new_cap;
+    r->cells = new_cs;
+  }
+
+  r->cells[r->count++] = c;
+  return true;
 }
